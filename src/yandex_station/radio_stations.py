ca419bbe15ed9d942343d55c34ec21f12ea081a7
0xcodepunk <1329474@gmail.com>
2025-04-12 23:22:51 +0300

    feat(streaming): добавлены новые FM-станции в список стриминга
    Поддержка следующих радиостанций:
    - Like FM 87.9
    - Восток FM 94.0
    - Звезда 95.6
    - Шансон 103.0
    - Восток России 103.7
    - Jam FM 102.5
    - Европа Плюс 106.2
    - Дорожное радио 96.0
    - Жара FM 100.5
    - Rock Arsenal 104.5
    - Hit FM 107.4
    - Capital FM 105.3
    - Remix FM 104.8
    - Comedy Radio 102.5

--- src/yandex_station/radio_stations.py
+++ src/yandex_station/radio_stations.py
@@ -20,6 +20,20 @@ class RadioStations(enum.Enum):
     radio7 = "https://radio7.hostingradio.ru:8040/radio7256.mp3"  # Radio 7 103.8  # noqa: E501
     avtoradio = "https://srv01.gpmradio.ru/stream/air/aac/64/100"  # Авторадио 90.3  # noqa: E501
     radio_dfm = "https://dfm.hostingradio.ru/dfm96.aacp"  # DFM 101.2  # noqa: E501
+    like_fm = "https://srv11.gpmradio.ru:8443/stream/air/aac/64/219"  # Like FM 87.9  # noqa: E501
+    vostok_fm = "https://stream2.n340.com:8443/18_vostok_64_reg_1?type=.aac&UID=0BB099281DC03DD138A95B51F428908F"  # Восток FM 94.0  # noqa: E501
+    zvezda = "https://zvezda-radio-rzv.mediacdn.ru/radio/zvezda/zvezda_128"  # Звезда 95.6  # noqa: E501
+    chanson = "https://chanson.hostingradio.ru:8041/chanson256.mp3"  # Шансон 103.0  # noqa: E501
+    vostok_rossii = "https://radiovostok.hopto.org/"  # Восток России 103.7
+    jam_fm = "https://online.jamfm.ru/jam?v=1744473935697"  # Jam FM 102.5
+    europa_plus = "https://ep256.hostingradio.ru:8052/europaplus256.mp3"  # Европа+ 106.2  # noqa: E501
+    dorognoe_radio = "https://dorognoe.hostingradio.ru/dorognoe"  # Дорожное радио 96.0  # noqa: E501
+    zhara = "https://hls-01-zharafm.hostingradio.ru/zharafm/112/playlist.m3u8"  # Жара FM 100.5  # noqa: E501
+    rock_arsenal = "https://online.rockarsenal.ru/rockwotalk?v=1744474589441"  # Rock Arsenal 104.5  # noqa: E501
+    hit_fm = "https://hitfm.hostingradio.ru/hitfm96.aacp"  # Hit FM 107.4  # noqa: E501
+    capital_fm = "https://icecast-vgtrk.cdnvideo.ru/capitalfmmp3"  # Capital FM 105.3  # noqa: E501
+    remix_fm = "https://rmx.amgradio.ru/RemixFM"  # Remix FM 104.8  # noqa: E501
+    comedy_radio = "https://srv21.gpmradio.ru:8443/stream/air/aac/64/202"  # Comedy Radio 102.5  # noqa: E501
 
 
 async def get_radio_stations(station_name: str):
